81b24cb8775720d80c02263556e5609b6bf9ee61
Nathan Knebusch <nateknebusch@gmail.com>
2018-09-20 12:58:46 -0700

    Worked on trying to figure out how to make all the units work together

--- Battle Arena Game/Super Soldier.cpp
+++ Battle Arena Game/Super Soldier.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <string>
-#include <ctime>
 #include "Super Soldier.h"
 
 int superSoldier::attack()
--- Battle Arena Game/main.cpp
+++ Battle Arena Game/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include "Tank.h"
+#include "Super Soldier.h"
+#include "Grunt.h"
+
+// tank
+char tank
+{
+	std
+};
+
+size_t teamA = 3;
+size_t teamB = 3;
 
 int main()
 {
-
+	
 
 	return 0;
 }
